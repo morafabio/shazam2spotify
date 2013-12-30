0aa79451dda8e760802b871edc0eff3a3e048d37
Fabio Mora <mail@fabio.mora.name>
2013-12-30 14:46:36 +0100

    scripts fix.

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+command_exists () {
+    type "$1" >/dev/null 2>&1 ;
+}
+
 download_composer () {
   OUTPUT_DIR="${2:-$(pwd)}"
 
@@ -7,9 +11,9 @@ download_composer () {
     mkdir "$OUTPUT_DIR"
   fi
 
-  if odb_command_exists "wget" ; then
+  if command_exists "wget" ; then
     wget -q -P "$OUTPUT_DIR" "$1"
-  elif odb_command_exists "curl" ; then
+  elif command_exists "curl" ; then
     ( cd "$OUTPUT_DIR" > /dev/null ; curl --silent -O "$1" ; )
   else
     echo "Cannot download $1 [missing wget or curl]"
